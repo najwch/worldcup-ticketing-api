b2a928427a36d6fbfd5ff46cc821aa34ab0b410a
Najwa CHELH <najwa.chelh9@etu.univ-lorraine.fr>
2026-03-09 07:21:48 +0100

    Actualisation

--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,12 @@ import { Hono } from "hono";
 console.log("Hello via Bun!");
 
 export const app = new Hono();
+
+app.get("/", (c) => {
+  return c.json(
+    {
+        success : true,
+        message : "World Cup Ticketing API",
+    },
+  );
+});
