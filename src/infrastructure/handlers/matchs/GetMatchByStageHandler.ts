176deaf9d9a3da3b1c364964ff53da872d9951be
Najwa CHELH <najwa.chelh9@etu.univ-lorraine.fr>
2026-03-30 10:12:10 +0200

    Premier commit du TD4

--- src/infrastructure/handlers/matchs/GetMatchByStageHandler.ts
+++ src/infrastructure/handlers/matchs/GetMatchByStageHandler.ts
@@ -1,17 +1,17 @@
 import type { Context } from "hono";
 import { matchs } from "@infrastructure/mock/matchs";
+import { HTTPException } from "hono/http-exception";
 
 export class GetMatchByStageHandler {
     async handle(c : Context){
         const stage = c.req.query("stage");
 
+        // dans le cas où la valeur du stage ne fait pas partie des valeurs autorisées 
         const validStages = ["group", "round_of_32", "round_of_16", "quarter_finals", "semi_finals", "third_place", "final"];
-
         if (!stage || !validStages.includes(stage.toLowerCase())) {
-            return c.json({
-                success: false,
-                error: "Le stage est incorrect."
-            }, 400);
+            throw new HTTPException(400, {
+                message : "La valeur de stage ne fait pas partie des valeurs autorisées."
+            });
         }
 
         const matchsFiltres = matchs.filter(m => 
@@ -21,7 +21,7 @@ export class GetMatchByStageHandler {
         return c.json({
             success: true,
             message: `Matchs pour l'étape : ${stage}`,
-            data: matchsFiltres
+            data: matchsFiltres // renvoie la liste des matchs filtrés
         }, 200);
     }
 }
